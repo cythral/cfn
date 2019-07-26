e2c71b91012f4318697ba10ac2bb2274587b26bb
Talen Fisher <tqfisher@iastate.edu>
2019-07-26 00:40:55 -0500

    add HttpClientProvider property

---
+++ src/DefaultHttpClientProvider.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Cythral.CloudFormation.CustomResource {
+    public class DefaultHttpClientProvider : IHttpClientProvider {
+        public HttpClient Provide() {
+            return new HttpClient();
+        }
+    }
+}
---
+++ src/IHttpClientProvider.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Cythral.CloudFormation.CustomResource {
+    public interface IHttpClientProvider {
+        HttpClient Provide();
+    }
+}
