40d7bbc2b27e315def5d922046ebd09ab88ce513
Talen Fisher <talen.fisher@cythral.com>
2019-10-31 13:26:00 -0500

    fix core

--- src/Core/Core.cs
+++ src/Core/Core.cs
@@ -1,27 +1,29 @@
 using System;
 using System.Threading.Tasks;
 using Amazon;
+using Amazon.Lambda;
+using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.ApplicationLoadBalancerEvents;
 
 namespace Cythral.CloudFormation {
     public class Core {
         public static async Task Main(string[] args) {
-            
+            var serializer = new JsonSerializer();
             var handler = Environment.GetEnvironmentVariable("_HANDLER");
 
             switch(handler) {
-                case "GithubWebhook": await Bootstrap(Webhook.Handle); break;
+                case "GithubWebhook":
+                    Func<ApplicationLoadBalancerRequest, ILambdaContext, Task<ApplicationLoadBalancerResponse>> func = Webhook.Handle;
+                    using(var wrapper = HandlerWrapper.GetHandlerWrapper(func, serializer))
+                    using(var bootstrap = new LambdaBootstrap(wrapper)) {
+                        await bootstrap.RunAsync();
+                    }
+
+                    break;
+                
                 default: break;
-            }
-        }
-
-        private static async Task Bootstrap<T>(T handler) {
-            var serializer = new JsonSerializer();
-
-            using(var wrapper = HandlerWrapper.GetHandlerWrapper(handler, serializer))
-            using(var bootstrap = new LambdaBootstrap(wrapper)) {
-                await bootstrap.RunAsync();
             }   
         }
     }
