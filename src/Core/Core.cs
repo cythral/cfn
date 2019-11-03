008611dc26cebf9cb87faa21eace82b3c196db27
Talen Fisher <talen.fisher@cythral.com>
2019-11-03 12:54:07 -0600

    CYTHINF-14 Load Balancer Target (De)registration

--- src/Core/Core.cs
+++ src/Core/Core.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Threading.Tasks;
+
 using Amazon;
 using Amazon.Lambda;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.Json;
 using Amazon.Lambda.ApplicationLoadBalancerEvents;
+
+using Cythral.CloudFormation.Handlers;
 
 namespace Cythral.CloudFormation {
     public class Core {
@@ -15,8 +18,17 @@ namespace Cythral.CloudFormation {
 
             switch(handler) {
                 case "GithubWebhook":
-                    Func<ApplicationLoadBalancerRequest, ILambdaContext, Task<ApplicationLoadBalancerResponse>> func = Webhook.Handle;
-                    using(var wrapper = HandlerWrapper.GetHandlerWrapper(func, serializer))
+                    Func<ApplicationLoadBalancerRequest, ILambdaContext, Task<ApplicationLoadBalancerResponse>> webhookHandler = Webhook.Handle;
+                    using(var wrapper = HandlerWrapper.GetHandlerWrapper(webhookHandler, serializer))
+                    using(var bootstrap = new LambdaBootstrap(wrapper)) {
+                        await bootstrap.RunAsync();
+                    }
+
+                    break;
+
+                case "UpdateTargets":
+                    Func<UpdateTargetsHandler.Request, ILambdaContext, Task<UpdateTargetsHandler.Response>> targetsHandler = UpdateTargetsHandler.Handle;
+                    using(var wrapper = HandlerWrapper.GetHandlerWrapper(targetsHandler, serializer))
                     using(var bootstrap = new LambdaBootstrap(wrapper)) {
                         await bootstrap.RunAsync();
                     }
