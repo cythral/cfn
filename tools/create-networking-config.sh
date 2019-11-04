4a6b312935d538733eff70facd41bf32dd27bcde
Talen Fisher <talen.fisher@cythral.com>
2019-11-03 19:03:06 -0600

    fix load balancer dns name retrieval

--- tools/create-networking-config.sh
+++ tools/create-networking-config.sh
@@ -8,14 +8,10 @@ get_export() {
 }
 
 devPeeringConnectionId=$(get_export cfn-core DevPeeringConnectionId)
-devLoadBalancerDnsName=$(get_export cfn-metadata DevLoadBalancerDnsName)
 prodPeeringConnectionId=$(get_export cfn-core ProdPeeringConnectionId)
-prodLoadBalancerDnsName=$(get_export cfn-metadata ProdLoadBalancerDnsName)
 
 config={}
 config=$(echo $config | jq ".DevPeeringConnectionId=\"$devPeeringConnectionId\"") 
-config=$(echo $config | jq ".DevLoadBalancerDnsName=\"$devLoadBalancerDnsName\"")
-config=$(echo $config | jq ".ProdPeeringConnectionId=\"$prodPeeringConnectionId\"")
-config=$(echo $config | jq ".ProdLoadBalancerDnsName=\"$prodLoadBalancerDnsName\"") 
+config=$(echo $config | jq ".ProdPeeringConnectionId=\"$prodPeeringConnectionId\"")) 
 
 echo $config
