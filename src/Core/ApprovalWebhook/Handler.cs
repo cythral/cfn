86caf01310dd00a574c290df13528e7f2c91585e
Talen <talen.fisher@cythral.com>
2020-05-08 21:55:59 -0500

    fix response creation in approval webhook

--- src/Core/ApprovalWebhook/Handler.cs
+++ src/Core/ApprovalWebhook/Handler.cs
@@ -1,6 +1,9 @@
+using System.Net;
+using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
 using static System.Text.Json.JsonSerializer;
+using static System.Net.HttpStatusCode;
 
 using Cythral.CloudFormation.Aws;
 
@@ -17,23 +20,50 @@ namespace Cythral.CloudFormation.ApprovalWebhook
         public static async Task<ApplicationLoadBalancerResponse> Handle(ApplicationLoadBalancerRequest request, ILambdaContext context = null)
         {
             var client = stepFunctionsClientFactory.Create();
+            var action = request.QueryStringParameters["action"];
             var response = await client.SendTaskSuccessAsync(new SendTaskSuccessRequest
             {
                 TaskToken = request.QueryStringParameters["token"],
                 Output = Serialize(new
                 {
-                    Action = request.QueryStringParameters["action"]
+                    Action = action,
                 })
             });
 
             Console.WriteLine($"Send task success response: {Serialize(response)}");
 
+            var body = action == "approve" ? "approved" : "rejected";
+            return CreateResponse(OK, body: body);
+        }
+
+        private static ApplicationLoadBalancerResponse CreateResponse(HttpStatusCode statusCode, string contentType = "text/plain", string body = "")
+        {
+            string CreateStatusString()
+            {
+                var result = "";
+
+                foreach (var character in statusCode.ToString())
+                {
+                    if (Char.ToLower(character) == character)
+                    {
+                        result += character;
+                    }
+                    else
+                    {
+                        result += $" {character}";
+                    }
+                }
+
+                return result;
+            }
+
             return new ApplicationLoadBalancerResponse
             {
-                Body = Serialize(new
-                {
-                    Status = "OK"
-                })
+                StatusCode = (int)statusCode,
+                StatusDescription = $"{(int)statusCode}{CreateStatusString()}",
+                Headers = new Dictionary<string, string> { ["content-type"] = contentType },
+                Body = body,
+                IsBase64Encoded = false,
             };
         }
     }
