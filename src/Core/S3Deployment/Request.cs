e2ed073d27d00fc04721bd432c50abefa6f430a3
Talen <talen.fisher@cythral.com>
2020-05-13 20:16:38 -0500

    add s3 deployment function

---
+++ src/Core/S3Deployment/Request.cs
@@ -0,0 +1,8 @@
+namespace Cythral.CloudFormation.S3Deployment
+{
+    public class Request
+    {
+        public string ZipLocation { get; set; }
+        public string DestinationBucket { get; set; }
+    }
+}
