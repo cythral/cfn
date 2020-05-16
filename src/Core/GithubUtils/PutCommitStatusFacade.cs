93f9b26e99d65261a28e1db924375bff565d6f35
Talen <talen.fisher@cythral.com>
2020-05-15 23:11:32 -0500

    generate the sso url

--- src/Core/GithubUtils/PutCommitStatusFacade.cs
+++ src/Core/GithubUtils/PutCommitStatusFacade.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+
+using static System.Text.Json.JsonSerializer;
 
 using Octokit;
 
@@ -21,11 +25,33 @@ namespace Cythral.CloudFormation.GithubUtils
             await commitStatusClient.Create(request.GithubOwner, request.GithubRepo, request.GithubRef, new NewCommitStatus
             {
                 State = request.CommitState,
-                TargetUrl = $"https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/stackinfo?filteringText=&filteringStatus=active&viewNested=true&hideStacks=false&stackId={request.StackName}",
+                TargetUrl = GetSsoUrl(request),
                 Description = $"CloudFormation Deployment ${GetStatusDescription(request.CommitState)}",
                 Context = $"AWS CloudFormation ({request.EnvironmentName})"
             });
+        }
 
+        private static string GetSsoUrl(PutCommitStatusRequest request)
+        {
+            var stackUrl = $"https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/stackinfo?filteringText=&filteringStatus=active&viewNested=true&hideStacks=false&stackId={request.StackName}";
+
+            if (request.IdentityPoolId == null || request.GoogleClientId == null)
+            {
+                return stackUrl;
+            }
+
+            var nonceBytes = new byte[16];
+            using (var generator = new RNGCryptoServiceProvider())
+            {
+                generator.GetBytes(nonceBytes);
+            }
+
+            var nonce = string.Join("", nonceBytes.Select(x => $"{x:X2}"));
+            var destination = Uri.EscapeDataString(stackUrl);
+            var state = Serialize(new { identity_pool_id = request.IdentityPoolId, destination = destination });
+            var redirectUri = Uri.EscapeDataString("https://sso.brigh.id/redirect.html");
+
+            return $"https://accounts.google.com/o/oauth2/v2/auth?redirect_uri={redirectUri}&state={state}&client_id={request.GoogleClientId}&response_type=id_token&scope=profile+openid+email&nonce={nonce}";
         }
 
         private static string GetStatusDescription(CommitState state)
