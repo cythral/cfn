ca4918c627f0a803a4ff8889b67fb64294d92d87
Talen <talen.fisher@cythral.com>
2020-10-05 22:46:35 -0500

    Stack Deployment Statuses - Use Stack Names

--- src/Core/StackDeploymentStatus/Github/GithubStatusNotifier.cs
+++ src/Core/StackDeploymentStatus/Github/GithubStatusNotifier.cs
@@ -36,7 +36,7 @@ namespace Cythral.CloudFormation.StackDeploymentStatus.Github
                     State = state,
                     Description = description,
                     TargetUrl = $"https://sso.brigh.id/start/{environmentName}?destination={destination}",
-                    Context = $"AWS CloudFormation - {environmentName} ({repo})",
+                    Context = $"AWS CloudFormation - {environmentName} ({stackName})",
                 })
             });
         }
