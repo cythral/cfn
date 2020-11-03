736834db5ea817b4745c3a2736eee5762ff8d492
Talen <talen.fisher@cythral.com>
2020-11-03 17:11:54 -0600

    allow requests with x-amz-sns-topic-arn header

--- tools/create-networking-config.sh
+++ tools/create-networking-config.sh
@@ -11,13 +11,9 @@ get_export() {
 devPeeringConnectionId=$(get_export cfn-core DevPeeringConnectionId)
 prodPeeringConnectionId=$(get_export cfn-core ProdPeeringConnectionId)
 addresses=$(curl -so- https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes | map(select(.region == "us-east-1")) | map(.ip_prefix) | unique')
-addresses1=$(echo $addresses | jq -r '.[0:220] | join(",")')
-addresses2=$(echo $addresses | jq -r '.[221:] | join(",")')
 
 config={}
 config=$(echo $config | jq ".DevPeeringConnectionId=\"$devPeeringConnectionId\"") 
 config=$(echo $config | jq ".ProdPeeringConnectionId=\"$prodPeeringConnectionId\"")
-config=$(echo $config | jq ".AwsIpRanges1=\"$addresses1\"")
-config=$(echo $config | jq ".AwsIpRanges2=\"$addresses2\"")
 
 echo $config
