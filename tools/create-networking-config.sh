38be5c00d1457bf9c1e9426ebf030841aa1b9437
Talen Fisher <talen.fisher@cythral.com>
2020-11-03 16:55:57 -0600

    Split ranges into subgroups (#90)

--- tools/create-networking-config.sh
+++ tools/create-networking-config.sh
@@ -10,11 +10,14 @@ get_export() {
 
 devPeeringConnectionId=$(get_export cfn-core DevPeeringConnectionId)
 prodPeeringConnectionId=$(get_export cfn-core ProdPeeringConnectionId)
-addresses=$(curl -so- https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes | map(select(.region == "us-east-1" and .service == "AMAZON")) | map(.ip_prefix) | join(",")')
+addresses=$(curl -so- https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes | map(select(.region == "us-east-1")) | map(.ip_prefix) | unique')
+addresses1=$(echo $addresses | jq -r '.[0:220] | join(",")')
+addresses2=$(echo $addresses | jq -r '.[221:] | join(",")')
 
 config={}
 config=$(echo $config | jq ".DevPeeringConnectionId=\"$devPeeringConnectionId\"") 
 config=$(echo $config | jq ".ProdPeeringConnectionId=\"$prodPeeringConnectionId\"")
-config=$(echo $config | jq ".AwsIpRanges=\"$addresses\"")
+config=$(echo $config | jq ".AwsIpRanges1=\"$addresses1\"")
+config=$(echo $config | jq ".AwsIpRanges2=\"$addresses2\"")
 
 echo $config
