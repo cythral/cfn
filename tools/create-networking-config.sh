caa030db6c5e805c9814ff3127090af300fdc0d7
Talen Fisher <talen.fisher@cythral.com>
2020-12-02 17:46:37 -0600

    OSS Account Part 2: Setup VPC Peering (#106)

--- tools/create-networking-config.sh
+++ tools/create-networking-config.sh
@@ -10,10 +10,12 @@ get_export() {
 
 devPeeringConnectionId=$(get_export cfn-core DevPeeringConnectionId)
 prodPeeringConnectionId=$(get_export cfn-core ProdPeeringConnectionId)
+ossPeeringConnectionId=$(get_export cfn-core OssPeeringConnectionId)
 addresses=$(curl -so- https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes | map(select(.region == "us-east-1")) | map(.ip_prefix) | unique')
 
 config={}
 config=$(echo $config | jq ".DevPeeringConnectionId=\"$devPeeringConnectionId\"") 
 config=$(echo $config | jq ".ProdPeeringConnectionId=\"$prodPeeringConnectionId\"")
+config=$(echo $config | jq ".OssPeeringConnectionId=\"$ossPeeringConnectionId\"")
 
 echo $config
