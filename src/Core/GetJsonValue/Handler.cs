175f1023a8909e72f0e4c5cc3eab42616271b769
Talen Fisher <talen.fisher@cythral.com>
2021-06-22 23:35:52 -0500

    Log found values

--- src/Core/GetJsonValue/Handler.cs
+++ src/Core/GetJsonValue/Handler.cs
@@ -5,11 +5,22 @@ using System.Threading.Tasks;
 using Lambdajection.Attributes;
 using Lambdajection.CustomResource;
 
+using Microsoft.Extensions.Logging;
+
 namespace Cythral.CloudFormation.GetJsonValue
 {
     [CustomResourceProvider(typeof(Startup))]
     public partial class Handler
     {
+        private readonly ILogger<Handler> logger;
+
+        public Handler(
+            ILogger<Handler> logger
+        )
+        {
+            this.logger = logger;
+        }
+
         public Task<OutputData> Create(CustomResourceRequest<Properties> request, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
@@ -17,6 +28,7 @@ namespace Cythral.CloudFormation.GetJsonValue
 
             object? result = null;
             props?.Json?.TryGetValue(props?.Key ?? string.Empty, out result);
+            logger.LogInformation("Found value: {@value}", result);
             return Task.FromResult(new OutputData { Id = request.PhysicalResourceId ?? Guid.NewGuid().ToString(), Result = result });
         }
 
